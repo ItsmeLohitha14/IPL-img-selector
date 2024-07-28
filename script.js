40153448d094a5a5b865039c9970326109f4d4fe
ItsmeLohitha14 <itsmelohitha14@gmail.com>
2024-07-28 22:14:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let imageElement=document.getElementById("image")
+function MI() {
+    imageElement.src="https://im.rediff.com/cricket/2023/dec/20mi-squad.jpg"    
+}
+function GT(){
+    imageElement.src="https://thebridge.in/h-upload/2022/06/15/30456-ipl-media-rights.webp"
+}
+function CSK(){
+    imageElement.src="https://img.jagranjosh.com/images/2022/March/2632022/CSK_player_list_2022_stats_past_records.jpg"
+}    
+
+function LSG(){
+    imageElement.src="https://static.toiimg.com/thumb/msid-98102174,width-1280,height-720,resizemode-4/98102174.jpg"
+}
